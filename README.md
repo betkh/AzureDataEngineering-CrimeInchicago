6c2cf16d9690619f4ec4bcfb34175a9f648cbbd9
dfroslie-ndsu <111405021+dfroslie-ndsu@users.noreply.github.com>
2022-10-10 08:34:18 -0500

    Add ExploratoryAnalysis.py, misc updates.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <Tell the world about your project in this readme.  There should be sections that align with the data engineering lifecyle - Ingestion, Transformation, Serving - that serve as the design documentation.  >
 
 ## Ingestion
+<Design documentation for ingestion goes here.  Where does the data come from?  How is it ingested?  Where is it stored (be specific)?>
---
+++ src/ingestion/ExploratoryAnalysis.py
@@ -0,0 +1,5 @@
+# ExploratoryAnalysis.py - script to do EDA (exploratory data analysis) on the two primary data sets for the project.  Use pandas_profiling and exprt to <repo_root>\ingestion_analysis.
+
+# Note - Some data sources may have multiple files that you will use.  There is  likely a primary dataset with the most of the data; target that primary dataset for this analysis.
+
+# This file can be developed using Python Interactive in VS Code or as a straight script.
