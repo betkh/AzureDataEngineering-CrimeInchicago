612338caff9d02e11a5dc8398fed2c8d8ab39879
Bekalu <bekpostgrad@gmail.com>
2024-11-24 21:05:25 -0600

    refactored to ingest geo-json file than csv

--- src/ingestion/DataSet3_Socioeconomic.py
+++ src/ingestion/DataSet3_Socioeconomic.py
@@ -1,23 +1,19 @@
 
-from functions.setup import load_config, init_adls_directory, save_and_load_csv
-from functions.pull_data import fetch_data_from_api
-from functions.timeLabels import crimes_fileLabel, socio_fileLabel
-from functions.upload_ADLs import upload_dataframe_to_adls
+from functions.setup import load_config, init_adls_directory
+from functions.pull_data import fetch_geojson_from_api
+from functions.upload_ADLs import upload_geojson_to_adls
+import json
 
 
-def ingest_socioecon_areas_data(END_POINT="2ui7-wiq8.json",
-                                BASE_URL="https://data.cityofchicago.org/resource",
-                                MAX_RECORDS=300000,
-                                TIME_OUT=10,
-                                DELAY=1.5,
-                                SAVE_PATH='RawData/DataSet3'):
+def ingest_socioecon_areas_data(END_POINT="2ui7-wiq8.geojson",
+                                BASE_URL="https://data.cityofchicago.org/resource"):
     """
     Ingests the 'Socioeconomic areas' dataset and uploads to Azure Data Lake Storage (ADLS).
 
     About Data:
     Source #3: Socioeconomically Disadvantaged Areas - (STATIC source)
 
-        - API Endpoint	https://data.cityofchicago.org/resource/2ui7-wiq8.json
+        - API Endpoint	https://data.cityofchicago.org/resource/2ui7-wiq8.geojson
         - API Documentation	https://dev.socrata.com/foundry/data.cityofchicago.org/2ui7-wiq8
         - Data Owner	Department of Planning and Development
         - Date Created	October 13, 2022
@@ -28,7 +24,7 @@ def ingest_socioecon_areas_data(END_POINT="2ui7-wiq8.json",
         - 254K rows x 1 column
     """
 
-    print("DataSet3 ingestion - 'Socioeconomic Areas...'")
+    print("DataSet3 ingestion - 'Socioeconomically disadvantaged Areas...'")
 
     # Load API keys
     api_key_id, api_secret = load_config()
@@ -39,29 +35,26 @@ def ingest_socioecon_areas_data(END_POINT="2ui7-wiq8.json",
     # Construct the full URL
     url = f"{BASE_URL}/{END_POINT}"
 
-    # fetch_data_from_api(url, api_key_id, api_secret, columns=None, row_filter=None, max_records=100000, timeout=10, delay=1)
+    geojson_data = fetch_geojson_from_api(url, api_key_id, api_secret)
 
-    df = fetch_data_from_api(url, api_key_id, api_secret,
-                             max_records=MAX_RECORDS,
-                             timeout=TIME_OUT,
-                             delay=DELAY)
+    # Check if the response contains data
+    if geojson_data:
+        print("[Success] - GeoJSON data fetched successfully.")
+        print("GeoJSON data preview:")
+        # Pretty print the GeoJSON data
+        # print(json.dumps(geojson_data, indent=2))
+    else:
+        print("[Error] - GeoJSON data is empty or null.")
 
-    print("[Success] - Data fetch from API successful and data stored in df")
-    print("\nData insights:")
-    print(df.head())
-    print(df.shape)
-    print(f"Number of columns: {len(df.columns)}")
-    print(f"Number of rows: {len(df)}")
+        print("[Success] - Data fetch from API successful")
 
-    # Generate file label based on the date range
-    csv_fileLabel = socio_fileLabel(df, label_="socio_econ_areas")
+    #  file label
+    geojson_file_label = "socioecon_disadvantaged_Areas.geojson"
+    SAVE_PATH = 'RawData/DataSet3'
+    geojson_file_path = f"{SAVE_PATH}/{geojson_file_label}"
 
-    print("\n[Success] - Generated file label:", csv_fileLabel)
-
-    # save data as cvs
-    df_read = save_and_load_csv(df,
-                                SAVE_PATH,
-                                csv_fileLabel)
+    with open(geojson_file_path, "w") as f:
+        json.dump(geojson_data, f)  # Save GeoJSON data as a JSON file
 
     # Init Azure Data Lake storage client
     with open("sas.config") as f:
@@ -77,12 +70,10 @@ def ingest_socioecon_areas_data(END_POINT="2ui7-wiq8.json",
                                     dirName)
 
     # Upload to ADLS
-    upload_dataframe_to_adls(directory, df_read, csv_fileLabel)
+    upload_geojson_to_adls(directory, geojson_file_path, geojson_file_label)
 
 
 # Allow this script to be run independently or imported
 if __name__ == "__main__":
 
-    ingest_socioecon_areas_data(MAX_RECORDS=300000,
-                                DELAY=3.6,
-                                TIME_OUT=20)
+    ingest_socioecon_areas_data()
